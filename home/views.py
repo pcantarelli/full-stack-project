6d60c1b42f53219216bc794b450d7cc5d89bf580
Pedro Luiz Cantarelli Junior <pcantarelli@hotmail.com>
2021-07-13 14:34:10 +0100

    Update Gallery links on Homepage (HTMl and CSS)

--- home/views.py
+++ home/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from doodles.models import Doodles
+from gallery.models import GalleryImages
+
 
 # Create your views here.
 
@@ -9,8 +11,12 @@ def index(request):
     """
 
     doodles = Doodles.objects.all()
+    doodles = doodles.order_by('-updated_at')[:4]
+    gallery_images = GalleryImages.objects.all()
+    gallery_images = gallery_images.order_by('-updated_at')[:3]
 
     context = {
         'doodles': doodles,
+        'gallery_images': gallery_images,
     }
     return render (request, 'home/index.html', context)

2218f11b7cf4421e8b33bdf4bf31c8612e2ca706
Pedro Luiz Cantarelli Junior <pcantarelli@hotmail.com>
2021-07-26 21:37:43 +0100

    Added ALLOWED_HOSTS on Settings.py

--- tatzy_tattoo/settings.py
+++ tatzy_tattoo/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-21a^z=-&ln^ydvj=tlxxnyeq*(3#3a_5v8y2%x4t(@d1bdg)da
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['tatzy-tatoo.herokuapp.com', 'localhost']
 
 
 # Application definition

d4dd3bfb49c441939c6844362ad9f138114c9b10
Pedro Luiz Cantarelli Junior <pcantarelli@hotmail.com>
2021-07-26 22:10:57 +0100

    Fixed SECRET_KEY and DEBUG on Settings.py

--- tatzy_tattoo/settings.py
+++ tatzy_tattoo/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-21a^z=-&ln^ydvj=tlxxnyeq*(3#3a_5v8y2%x4t(@d1bdg)da'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['tatzy-tatoo.herokuapp.com', 'localhost']
 
